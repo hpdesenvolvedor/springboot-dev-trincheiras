d55fd58691953e3c3990c43aeb546f903a92dc85
Henrique <nuneshp2@gmail.com>
2024-08-14 22:20:36 -0300

    First Commit

---
+++ src/test/java/henriquenuneshp/anime_service/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package henriquenuneshp.anime_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
